bbab7f18248356a10236e93d4dc120c1295b6cb3
Max Skvortsov <maxbusters1387@gmail.com>
2023-08-02 01:16:01 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/test-sandbox/',
   plugins: [
     vue({
       template: { transformAssetUrls }
